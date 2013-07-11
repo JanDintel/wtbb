737c0dc145d3eaf050284e847a9c4aa93dbaf115
Jan van der Pas <janvanderpas@gmail.com>
2013-07-11 18:03:18 +0200

    Add validations to User and Event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,6 @@
 class Event < ActiveRecord::Base
   attr_accessible :event_id, :name, :ratio
+  
+  validates_uniqueness_of :event_id
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  validates_uniqueness_of :uid
+  
   def self.from_omniauth(auth)
     where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
       user.provider = auth.provider
